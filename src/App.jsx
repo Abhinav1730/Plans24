b384adeaca35307603930fb1d7e4a9ca6ad0bdb4
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-05-16 22:05:06 +0530

    Setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h2>Welcome</h2>
+      <Button>Abhinav</Button>
+    </>
+  )
+}
+
+export default App
