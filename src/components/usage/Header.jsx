37278f056e651863eaa8d94f460acef7aff17f5a
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-06-20 00:02:03 +0530

    Header and hero section of the page

--- src/components/usage/Header.jsx
+++ src/components/usage/Header.jsx
@@ -3,8 +3,8 @@ import { Button } from "../ui/button";
 
 function Header() {
   return (
-    <div className="p-3 shadow-sm flex justify-between items-center px-5">
-      <img src="/logo-plans24.png" className="w-24" />
+    <div className="p-3 shadow-sm flex justify-between items-center px-5 bg-inherit border rounded-2xl border-white">
+      <img src="/logo-plans24.png" className="w-24 rounded-full" />
       <div>
         <Button>Sign In</Button>
       </div>
--- src/components/usage/Hero.jsx
+++ src/components/usage/Hero.jsx
@@ -4,25 +4,30 @@ import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <div className="flex flex-col items-center mx-56 gap-9">
-      <h1 className="font-sans text-[35px] text-center mt-16">
-        Plan your perfect trip effortlessly with our AI travel planner –
-        <span className="text-red-700">smart, fast, </span> and{" "}
-        <span className="text-red-700">
-          tailored to your preferences and budget
-        </span>
-        .
-      </h1>
-      <p className="text-xl text-rose-500 text-center">
-        Discover destinations, create custom itineraries, and explore smarter —
-        all powered by AI to make your travel planning seamless and fun.
-      </p>
+    <div
+      className="w-full h-screen bg-cover bg-center flex flex-col items-center justify-center gap-9 rounded-2xl"
+      style={{ backgroundImage: "url('/public/place-1.jpg')" }}
+    >
+      <div className="flex flex-col items-center mx-56 gap-9">
+        <h1 className="font-sans text-[35px] text-center mt-16 border-red-100 rounded-3xl p-2 border bg-transparent text-white">
+          Plan your perfect trip effortlessly with our AI travel planner –
+          <span className="text-red-100 font-serif">smart, fast, </span> and{" "}
+          <span className="text-red-100 font-serif">
+            tailored to your preferences and budget
+          </span>
+          .
+        </h1>
+        <p className="text-xl bg-transparent text-center text-gray-100 border border-red-100 rounded-3xl p-2 font-serif">
+          Discover destinations, create custom itineraries, and explore smarter
+          — all powered by AI to make your travel planning seamless and fun.
+        </p>
 
-      <Link to={"/create-trip"}>
-        <Button className="bg-rose-300 text-black font-sans hover:bg-red-700 hover:text-white">
-          Get Started
-        </Button>
-      </Link>
+        <Link to={"/create-trip"}>
+          <Button className="bg-black text-white font-sans hover:bg-red-700 hover:text-white">
+            Get Started
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
