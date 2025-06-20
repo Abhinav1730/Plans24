385ee465e0f789daf05d5985bdaacbb4b3fc5cc8
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-06-20 16:43:41 +0530

    Completion of Lamding Page of the Form for the trips.

---
+++ src/constants/options.jsx
@@ -0,0 +1,54 @@
+export const SelectTravelList = [
+  {
+    id: 1,
+    title: "Solo",
+    description: "A solo traveler fond of exploration and fun alone.",
+    icon: "✈️",
+    people: "1",
+  },
+  {
+    id: 2,
+    title: "A Couple",
+    description: "Exploring together brings couples closer than ever.",
+    icon: "🥂",
+    people: "2",
+  },
+  {
+    id: 3,
+    title: "Family",
+    description:
+      "A Group of fun loving people who love adventure and exploration.",
+    icon: "🏠",
+    people: "3 to 5 People",
+  },
+  {
+    id: 4,
+    title: "Friends",
+    description: "Friends who travel together creates the best memories.",
+    icon: "🤝",
+    people: "5 to 10 People",
+  },
+];
+
+export const SelectBudgetOptions = [
+  {
+    id: 1,
+    title: "Economical",
+    description: "Budget friendly and conscious to your pocket",
+    icon: "💵",
+  },
+  {
+    id: 2,
+    title: "Moderate",
+    description: "Keeping cost on the average side",
+    icon: "🪙",
+  },
+  {
+    id: 3,
+    title: "Luxury",
+    description: "No worries about the cost , enjoyment matters",
+    icon: "💰",
+  },
+];
+
+export const AI_PROMPT = "Generate Travel Plan for Location : {location}";
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import OSMSearchBox from "./OSMSearchBox";
+import { Input } from "../components/ui/input.jsx";
+import { SelectBudgetOptions, SelectTravelList } from "../constants/options";
+import { Button } from "../components/ui/button";
 
 const backGroundImages = [
   "/place-5.jpg",
@@ -19,13 +22,13 @@ function CreateTrip() {
       setCurrentBackGroundImageIndex((prevIndex) =>
         prevIndex === backGroundImages.length - 1 ? 0 : prevIndex + 1
       );
-    }, 30000);
+    }, 10000);
 
     return () => clearInterval(interval); //for cleanup the mounted ones
   }, []);
   return (
     <div
-      className="w-full h-screen bg-cover bg-center flex flex-col items-center justify-center gap-9 transition-all duration-1000 ease-in-out rounded-2xl"
+      className="w-full min-h-screen bg-cover bg-center flex flex-col items-center justify-center gap-9 transition-all duration-1000 ease-in-out rounded-2xl"
       style={{
         backgroundImage: `url(${backGroundImages[currentBackGroundImageIndex]})`,
       }}
@@ -40,9 +43,9 @@ function CreateTrip() {
           craft the perfect itinerary tailored just for you!
         </p>
 
-        <div className="mt-20">
+        <div className="mt-20 flex flex-col gap-9">
           <div>
-            <h2 className="text-2xl my-3 font-medium">
+            <h2 className="text-2xl my-3 font-medium text-white font-serif">
               What is your Destination?
             </h2>
             <OSMSearchBox onSelect={(place) => setSelectedDestination(place)} />
@@ -52,7 +55,65 @@ function CreateTrip() {
               </p>
             )}
           </div>
+          <div>
+            <h2 className="text-2xl my-3 font-medium text-white font-serif">
+              How Many Days are you Planning the Trip?
+            </h2>
+            <Input
+              placeholder={"Example:3"}
+              type="number"
+              className="rounded-3xl text-gray-100 border border-black bg-white text-2xl"
+            />
+          </div>
         </div>
+        <div className="mt-10">
+          <h2 className="text-2xl my-3 font-medium text-white font-serif">
+            What is your Budget?
+          </h2>
+          <h3 className="text-xl my-3 font-medium text-white font-serif">
+            The Budget is exclusively for activities and dining purposes.
+          </h3>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectBudgetOptions.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border rounded-lg hover:shadow-xl cursor-pointer"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-white text-lg font-serif">
+                  {item.title}
+                </h2>
+                <h2 className="text-sm text-white font-serif">
+                  {item.description}
+                </h2>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="mt-10">
+          <h2 className="text-2xl my-3 font-medium text-white font-serif">
+            With whom do you plan on travelling with on your next Adventure?
+          </h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectTravelList.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border rounded-lg hover:shadow-xl cursor-pointer"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-white font-serif text-lg">
+                  {item.title}
+                </h2>
+                <h2 className="text-sm text-white font-serif">
+                  {item.description}
+                </h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="my-10 justify-end flex">
+        <Button className="text-xl font-serif">Generate the Trip</Button>
       </div>
     </div>
   );
