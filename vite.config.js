5011f545a45bf7812f6cb83d7b763f59d1287f8c
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-06-22 02:05:20 +0530

    Minor bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      "/api/nominatim":{
+// vite.config.js
+export default {
+  server: {
+    proxy: {
+      '/api/nominatim': {
         target: 'https://nominatim.openstreetmap.org',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/nominatim/, '')
+        rewrite: path => path.replace(/^\/api\/nominatim/, '')
       }
-      }
+    }
   }
-})
+};
+
