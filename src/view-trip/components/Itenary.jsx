b2217c60d1d3e1deef8c838a8e17ea2b83e73812
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-06-22 19:28:25 +0530

    Minor Bug Fixes

--- src/view-trip/components/Itenary.jsx
+++ src/view-trip/components/Itenary.jsx
@@ -3,20 +3,21 @@ import PlacesCard from "./PlacesCard";
 
 function Itenary({ trip }) {
   return (
-    <div className="font-poppins">
+    <div className="font-poppins px-4 sm:px-6 md:px-10">
       <h2 className="font-serif font-bold text-2xl mt-5 flex items-start">
-        Your Recommended Itenary
+        Your Recommended Itinerary
       </h2>
+
       <div>
         {trip?.tripData?.itinerary?.map((item, index) => (
-          <div key={index}>
-            <h2 className="font-serif font-extrabold text-lg mt-5">
+          <div key={index} className="mt-6">
+            <h2 className="font-serif font-extrabold text-lg mb-3">
               Day {item?.day}
             </h2>
-            <div className="grid gap-5 md:grid-cols-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
               {item?.plan?.map((plan, planIndex) => (
-                <div className="my-3" key={planIndex}>
-                  <h2 className="font-serif font-medium text-sm text-red-600">
+                <div className="my-1" key={planIndex}>
+                  <h2 className="font-serif font-medium text-sm text-red-600 mb-1">
                     {plan?.timeSlot}
                   </h2>
                   <PlacesCard plan={plan} />
@@ -31,4 +32,3 @@ function Itenary({ trip }) {
 }
 
 export default Itenary;
-
